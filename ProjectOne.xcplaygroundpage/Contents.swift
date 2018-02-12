9981556780c27a151fc817b4f4bc68969b44a88b
mhortillano <mhortillano@github.com>
2018-02-12 10:48:55 -1000

    fixed

---
+++ ProjectOne.xcplaygroundpage/Contents.swift
@@ -0,0 +1,49 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//Variable Coding Challenge #1
+var firstString = "Project One!"
+
+let numberOfCodingChallenges = 10
+var secondString = 1
+
+var thirdString = 2
+
+print("This is coding challenge \(thirdString)!")
+
+
+
+//Variable Coding challange #2
+
+let fahrenheit = 50 as Int
+
+let celsius = (fahrenheit - 32) * 5/9 as Int
+
+print("Today is \(celsius) degrees celsius!")
+
+
+//Boolean Variable Coding Challenge #1
+
+var numberOne = 1
+
+var numberTwo = 2
+
+var numberThree = numberOne == numberTwo
+
+
+//If statement Coding Challange #1
+
+let stringOne = "God, I'm clueless."
+
+let stringTwo = "I need assitance."
+
+if stringOne > stringTwo {
+    print("True");
+}
+else if stringTwo > stringOne {
+    print("False.");
+    
+}
